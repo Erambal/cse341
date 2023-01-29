4b8fd0659e40f93c139f07a35fa87aba649e090f
Erambal <k.rambal96@gmail.com>
2023-01-28 22:35:46 -0800

    Test 2

--- Week 4 Personal/swagger.js
+++ Week 4 Personal/swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Temple API'
     },
     host: 'week-4-personal.onrender.com',
-    schemes: ['http']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
